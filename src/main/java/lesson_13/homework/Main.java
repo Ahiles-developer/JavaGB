ecea2dc63b78315f7a3ff67b5827fd19e3ef65e7
Ahiles <yakushev_v@rambler.ru>
2021-05-23 19:43:16 +0300

    lesson_13.homework

---
+++ src/main/java/lesson_13/homework/Main.java
@@ -0,0 +1,43 @@
+package lesson_13.homework;
+
+/*
+Перенести приведенный в методичке (раздел практического задания) код в новый проект, где мы организуем гонки.
+Все участники должны стартовать одновременно, несмотря на разное время подготовки.
+В тоннель не может одновременно заехать больше половины участников (условность).
+Попробуйте все это синхронизировать.
+Первый участник, пересекший финишную черту, объявляется победителем (в момент пересечения этой самой черты).
+Победитель должен быть только один (ситуация с 0 или 2+ победителями недопустима).
+Когда все завершат гонку, нужно выдать объявление об окончании.
+Можно корректировать классы (в том числе конструктор машин) и добавлять объекты классов из пакета java.util.concurrent.
+ */
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class Main {
+
+    public static final int CARS_COUNT = 4;
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        CyclicBarrier cb = new CyclicBarrier(5);
+        CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cb, cdl);
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+        try {
+            cb.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            cb.await();
+            cb.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
